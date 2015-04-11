5339a0b66c14ac1d9fcb0c93dd74a85ad3977caf
saasbook <saasbook@saasbook>
2015-04-11 18:02:37 -0300

    added add cat function

---
+++ Petquest/app/models/cat.rb
@@ -0,0 +1,3 @@
+class Cat < ActiveRecord::Base
+  attr_accessible :Activity, :Age, :Anthelmintic, :Body, :Body, :Condition, :Flea, :Flooding, :Gender, :History, :Level, :Pet, :Score, :Sterilization, :Time, :Time, :Time, :Type, :Vaccination, :Vitro, :Weight, :name
+end
