a1fa6739a2b0fe085659cb70c7f800681cf0a246
kexin chen <kexinchen7@gmail.com>
2015-04-14 22:12:21 -0500

    delete Petquest folder, create petQuest folder

--- Petquest/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Petquest::Application.config.secret_token = '766dda1283b637e64511e2fdb0a76d108b49a796a2b4124ddb7a594d5a394d85f285fbe5308e7670a8155171469ad7dc0d07640075ad54fb9e8644bdd3f3114e'
