6094b26408b11911808aabee7345658e40ef3571
kexin chen <kexinchen7@gmail.com>
2015-04-21 21:46:37 -0500

    athletes deleted, add a dashboard

--- petQuest/test/controllers/welcome_controller_test.rb
+++ petQuest/test/controllers/welcome_controller_test.rb
@@ -6,4 +6,29 @@ class WelcomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get faqs" do
+    get :faqs
+    assert_response :success
+  end
+
+  test "should get pricing" do
+    get :pricing
+    assert_response :success
+  end
+
+  test "should get features" do
+    get :features
+    assert_response :success
+  end
+
 end
