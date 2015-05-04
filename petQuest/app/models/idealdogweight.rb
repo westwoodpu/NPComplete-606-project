76d76a06e19e3d4cc7ef4ee3df9ebd474f690c60
Jeremy <jeremy4tzou@gmail.com>
2015-05-03 21:49:45 -0500

    added dog breed and cat bree import

--- petQuest/app/models/idealdogweight.rb
+++ petQuest/app/models/idealdogweight.rb
@@ -1,2 +1,7 @@
 class Idealdogweight < ActiveRecord::Base
+def self.import(file)
+		CSV.foreach(file.path, headers: true) do |row|
+			Idealdogweight.create! row.to_hash
+		end
+	end
 end
--- petQuest/config/routes.rb
+++ petQuest/config/routes.rb
@@ -8,8 +8,16 @@ Rails.application.routes.draw do
     collection {post :import }
   end
 
-  resources :recipes, except: :add
+     resources :idealdogweights do
+    collection {post :import }
+  end
+  resources :idealcatweights do
+    collection {post :import }
+  end
 
+
+  resources :recipes, except: :add
+   
 
   get 'recipes/:id/add(.:format)' => 'foods#add', :as => :add
 
@@ -32,8 +40,8 @@ Rails.application.routes.draw do
    get 'calculate' => 'foods#calculate', :as => :calculate# to calculate the ME for pet(:id)
    post 'calculate' => 'foods#calculate', :as => :calculatemore# to calculate the ME for pet(:id)
   # You can have the root of your site routed with "root"
-    resources :idealdogweights
-   resources :idealcatweights
+
+
 
    root 'welcome#index'
 
